df492a76e8cd3ae161c672c60a6adc41f4295d86
Christopher Truett <chris.truett@gmail.com>
2014-03-19 22:25:32 -0400

    Initial upload on new repo. Had to switch around repo names.

---
+++ FeaturePresentation.py
@@ -0,0 +1,33 @@
+import sublime, sublime_plugin
+
+class FeaturePresentation(sublime_plugin.TextCommand):
+	def run(self, edit):
+
+		activated = self.view.settings().get('infocus')
+
+		if activated == None:
+			self.view.settings().set('infocus', False)
+
+		for region in self.view.sel():
+			regions = []
+
+			midpoint = region.begin() + region.size()/2
+
+			ss = self.view.text_point(self.view.rowcol(region.begin())[0] - 1, 0)
+			se = self.view.text_point(self.view.rowcol(region.end())[0] + 1, 0)
+
+			tr = sublime.Region(0,ss - 1)
+			br = sublime.Region(se,self.view.size())
+
+			regions = [tr,br]
+
+
+			if activated == False or None:
+				self.view.fold(regions)
+				self.view.settings().set('infocus', True)
+
+			if activated == True:
+				self.view.unfold(regions)
+				self.view.settings().set('infocus', False)
+
+			self.view.show_at_center(midpoint)
---
+++ README.md
@@ -0,0 +1,13 @@
+
+Feature Presentation
+====================
+
+This is a simple plugin to toggle everything around the currently selected text. The goal is to allow you to focus on a single bit of whatever document you are editing.
+
+### Installation
+
+You should have a Data/Packages folder in your Sublime Text install directory. All you need to do is download the plugin and put the plugin folder in the Packages folder.
+
+### Keybindings
+
+Feature Presentation is mapped to `f8` by default.
