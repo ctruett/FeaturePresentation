e1d1b9c0fd67727a7ef3dfb245ea570e683a99e7
Christopher Truett <christophertruett@chris-pc.gateway.2wire.net>
2015-04-15 10:18:42 -0400

    Added basic functionality with a settings preference to restore it.

--- FeaturePresentation.py
+++ FeaturePresentation.py
@@ -6,24 +6,29 @@ file_name = ''
 region = ''
 text = ''
 otex = ''
+basicmode = None
 
 
 class capture(sublime_plugin.EventListener):
 
     def on_pre_close(self, view):
 
-        # If we're reading a scratch, go ahead and process the changes
-        if view.is_scratch() is True:
+        if basicmode is None or False:
 
-            # Get the scratchpad's contents
-            global text
-            text = view.substr(sublime.Region(0, view.size()))
+            # If we're reading a scratch, go ahead and process the changes
+            if view.is_scratch() is True:
 
-            # Get the old view from our window object, and switch to it
-            oldview = sublime.active_window().find_open_file(file_name)
+                # Get the scratchpad's contents
+                global text
+                text = view.substr(sublime.Region(0, view.size()))
 
-            # Replace text in original buffer
-            oldview.run_command('fp_replace')
+                # Get the old view from our window object, and switch to it
+                oldview = sublime.active_window().find_open_file(file_name)
+
+                # Replace text in original buffer
+                oldview.run_command('fp_replace')
+                return
+
             return
 
 
@@ -37,21 +42,57 @@ class feature_presentation(sublime_plugin.TextCommand):
 
     def run(self, edit):
 
-        # Store filename
-        global file_name
-        file_name = self.view.file_name()
+        global basicmode
+        basicmode = self.view.settings().get("fp_basic")
 
-        # Get selection as region
-        global region
-        region = sublime.Region(self.view.sel()[0].begin(),
-                                self.view.sel()[0].end())
+        if basicmode is True:
+            activated = self.view.settings().get('infocus')
 
-        # Store original text
-        global otex
-        otex = self.view.substr(region)
+            for region in self.view.sel():
+                regions = []
 
-        # Create new view for focused text
-        self.clone_text()
+                midpoint = region.begin() + region.size() / 2
+
+                ss = self.view.text_point(self.view.rowcol(region.begin())[0] - 1, 0)
+                se = self.view.text_point(self.view.rowcol(region.end())[0] + 1, 0)
+
+                tr = sublime.Region(0, ss - 1)
+                br = sublime.Region(se, self.view.size())
+
+                regions = [tr, br]
+
+                if activated is None:
+                    self.view.fold(regions)
+                    self.view.settings().set('infocus', True)
+
+                if activated is False:
+                    self.view.fold(regions)
+                    self.view.settings().set('infocus', True)
+
+                if activated is True:
+                    self.view.unfold(regions)
+                    self.view.settings().set('infocus', False)
+
+                self.view.show_at_center(midpoint)
+            return
+
+        if basicmode is None or False:
+            # Store filename
+            global file_name
+            file_name = self.view.file_name()
+
+            # Get selection as region
+            global region
+            region = sublime.Region(self.view.sel()[0].begin(),
+                                    self.view.sel()[0].end())
+
+            # Store original text
+            global otex
+            otex = self.view.substr(region)
+
+            # Create new view for focused text
+            self.clone_text()
+            return
 
     def clone_text(self):
 
